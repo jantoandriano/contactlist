13341b9fee37123aff51ea5e4aece8667442474e
jantoandriano <motulojanto@gmail.com>
2024-03-10 17:51:11 +0700

    add empty state component to contact list component

--- src/components/contact-list/contact-list.tsx
+++ src/components/contact-list/contact-list.tsx
@@ -1,21 +1,35 @@
-import { useGetContacts } from "../../services/useGetContacts"
+import React from "react";
+import { Contact, useGetContacts } from "../../services/useGetContacts"
 import { Card } from "../card/card"
 import { Loader } from "../loader/loader"
 import "./contact-list.css"
+import { filterData } from "../../helper"
+import { EmptyState } from "../empty-state/empty-state";
 
-export const ContactList = () => {
+interface Props {
+    query: string;
+}
+
+export const ContactList: React.FC<Props> = ({ query }) => {
     const { data: contacts, isLoading } = useGetContacts()
+    const filteredData = filterData(contacts?.data, query)
 
-    if (isLoading && !contacts) {
+    if (isLoading) {
         return (
             <Loader />
+        )
+    }
+
+    if (!filteredData) {
+        return (
+            <EmptyState message="You haven't added any contact" path="/favorites" />
         )
     }
 
     return (
         <>
             <div className="contact__list__container">
-                {contacts?.data.map(contact => {
+                {filteredData.map((contact: Contact) => {
                     return (
                         <Card {...contact} key={contact.id} />
                     )
